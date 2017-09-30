5bc8cd3e87e624993170cfd7ab88b60f89090e22
iosdeveloper <morrison.andre@gmail.com>
2017-09-30 14:37:04 -0400

    Transition project to php

---
+++ footer.php
@@ -0,0 +1,22 @@
+							<!-- Footer -->
+							<section id="footer" class="special">
+								<ul class="icons">
+									<li><a href="index.html">Home</a></li>
+									<li><a href="details.html">Details</a></li>
+									<li><a href="registration.html">Registration</a></li>
+								</ul>
+								<p class="copyright">&copy; SNHU-a-Palooza</p>
+							</section>
+
+					</div>
+					
+			</div>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
