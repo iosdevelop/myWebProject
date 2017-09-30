b2a63787d5abd7f11c2f2cbdf6d2a39b785520f7
iosdeveloper <morrison.andre@gmail.com>
2017-09-30 14:39:05 -0400

    transition to php

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include_once "header.php";?>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a aria-label="Southern New Hampshire University Music Festival" href="#">SNHU Music Festival</a></h1>
+						<nav class="links">
+							<ul>
+								<li><a aria-label="Home Page Link" href="index.php">Home</a></li>
+								<li><a aria-label="Details page Link" href="details.php">Details</a></li>
+								<li><a aria-label="visit the registration link" href="registration.php">Registration</a></li>                            
+						  </ul>
+						</nav>
+						<nav class="main">
+							<ul>
+								<li class="menu">
+									<a class="fa-bars" href="#menu">Menu</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
+				<!-- Menu -->
+					<section id="menu">
+						<!-- Links -->
+							<section>
+								<ul class="links">
+									<li>
+										<a aria-label="home page link" href="index.php">
+											<h3>home</h3>
+											<p>main feature page</p>
+										</a>
+									</li>
+									<li>
+										<a aria-label="Learn more details about event" href="details.php">
+											<h3>Details</h3>
+											<p>information about main event</p>
+										</a>
+									</li>
+									<li>
+										<a aria-label="Registration page" href="registration.php">
+											<h3>registration</h3>
+											<p>enter registration details</p>
+									</a></li>
+								</ul>
+							</section>
+					</section>
+				<!-- Main -->
+					<div id="main">
+						<!-- Post -->
+							<article class="post">
+								<header>
+									<div class="title">
+										<h2><a aria-label="The home page link" href="index.php">SNHU-a-palooza The Concert</a></h2>
+										<p>Register and print your tickets today.</p>
+									</div>
+								</header>
+								<a aria-label="Featurd Image Advertising the Free Concert" href="#" class="image featured"><img src="images/Pic01Main.jpg" alt="SNHU-a-Palooza the Concert" /></a>
+								<p>SNHU is hosting a Concert series in Woodstock, NY.  Use the calendar to see the next available concert dates and location.  Use this website to get details and register for each event.</p>
+								<footer>
+									<ul class="actions">
+										<li><a aria-label="continue reading link" href="details.php" class="button big">Continue Reading</a></li>
+									</ul>
+								</footer>
+							</article>
+<?php include_once "footer.php"; ?>
