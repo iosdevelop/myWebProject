8a7fe03921a997b46f855748c0a820eba408f549
iosdeveloper <morrison.andre@gmail.com>
2017-10-08 19:10:15 -0400

    Added the details page with new style css.

---
+++ details.php
@@ -0,0 +1,26 @@
+<?php include_once "header.php";?>
+<article class="post">
+								<header>
+									<div class="title">
+										<h2><a aria-label="The home page link" href="index.php">Details</a></h2>
+										<p>Register and print your tickets today.</p>
+									</div>
+								</header>
+								
+								<p>SNHU is hosting a Concert series in Woodstock, NY.  Use the calendar to see the next available concert dates and location.  Use this website to get details and register for each event.</p>
+								<!-- Event Details -->
+							<div class="event-details">
+								<img src="images/when.jpg" alt="When Image"> <p>First Friday of every month.</p>
+								<img src="images/where.jpg" alt="Where"><p>Farmers Field in City of Woodstock, NY.</p>
+								<img src="images/free.jpg" alt="Free"><p>Admission is free but registration is required.</p>
+							</div>
+							<!-- Google Calendar -->
+							<div class="google-calendar">
+								<iframe src="GoogleCalendarWidget/calendar.html"></iframe>
+							</div>
+								<footer>
+									
+								</footer>
+							</article>
+						
+<?php include_once "footer.php"; ?>
--- header.php
+++ header.php
@@ -6,7 +6,57 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+		
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
 	<body>
+				<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a aria-label="Southern New Hampshire University Music Festival" href="#">SNHU Music Festival</a></h1>
+						<nav class="links">
+							<ul>
+								<li><a aria-label="Home Page Link" href="index.php">Home</a></li>
+								<li><a aria-label="Details page Link" href="details.php">Details</a></li>
+								<li><a aria-label="visit the registration link" href="registration.php">Registration</a></li>
+						  </ul>
+						</nav>
+						<nav class="main">
+							<ul>
+								<li class="menu">
+									<a class="fa-bars" href="#menu">Menu</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
+				<!-- Menu -->
+					<section id="menu">
+						<!-- Links -->
+							<section>
+								<ul class="links">
+									<li>
+										<a aria-label="home page link" href="index.php">
+											<h3>home</h3>
+											<p>main feature page</p>
+										</a>
+									</li>
+									<li>
+										<a aria-label="Learn more details about event" href="details.php">
+											<h3>Details</h3>
+											<p>information about main event</p>
+										</a>
+									</li>
+									<li>
+										<a aria-label="Registration page" href="registration.php">
+											<h3>registration</h3>
+											<p>enter registration details</p>
+									</a></li>
+								</ul>
+							</section>
+					</section>
+				<!-- Main -->
+				<div id="main">
